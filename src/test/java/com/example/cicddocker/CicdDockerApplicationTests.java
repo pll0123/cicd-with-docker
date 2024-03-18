df09448b3df663094b01b2c230df7b20e0cee207
plll0123 <jsh951227@gmail.com>
2024-03-19 05:00:23 +0900

    init

---
+++ src/test/java/com/example/cicddocker/CicdDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicddocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
